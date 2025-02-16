2ce94d873e4568e100f3c1a6cd0d9d5f8c9e61bb
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-16 21:10:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
-  // your code here
+   char c='';
+	for(let i =0;i<text.length;i++){
+		if(text[i]!=' '){
+			c=text[i];
+			break;
+		}
+	}
+	return c;
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
