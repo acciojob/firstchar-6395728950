47a7a018ae60b14cf225046483d3b0c3ce18f0ee
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-16 21:13:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function firstChar(text) {
-   char c='';
-	for(let i =0;i<text.length;i++){
-		if(text[i]!=' '){
-			c=text[i];
+   let c = null;
+	for(let i =0; i<text.length; i++){
+		if(text[i] !== ' '){
+			c = text[i];
 			break;
 		}
 	}
-	return c;
-}
+	return c === null ? '' : c;
+};
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
